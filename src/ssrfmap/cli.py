c3f2aa346634f2e0e11b1d2b38b7fef7f94fbf69
Akshath Kothari <akshath.kothari@levo.ai>
2021-10-15 17:50:13 +0530

    Update commands in help
    Update metadata for PyPI.
    
    Signed-off-by: Akshath Kothari <akshath.kothari@levo.ai>

--- src/ssrfmap/cli.py
+++ src/ssrfmap/cli.py
@@ -17,11 +17,11 @@ def display_banner():
 
 def parse_args():
     example_text = '''Examples:
-    python ssrfmap.py -r data/request2.txt -p url -m portscan
-    python ssrfmap.py -r data/request.txt -p url -m redis
-    python ssrfmap.py -r data/request.txt -p url -m portscan --ssl --uagent "SSRFmapAgent"
-    python ssrfmap.py -r data/request.txt -p url -m redis --lhost=127.0.0.1 --lport=4242 -l 4242
-    python ssrfmap.py -r data/request.txt -p url -m readfiles --rfiles 
+    ssrfmap -r data/request2.txt -p url -m portscan
+    ssrfmap -r data/request.txt -p url -m redis
+    ssrfmap -r data/request.txt -p url -m portscan --ssl --uagent "SSRFmapAgent"
+    ssrfmap -r data/request.txt -p url -m redis --lhost=127.0.0.1 --lport=4242 -l 4242
+    ssrfmap -r data/request.txt -p url -m readfiles --rfiles
     '''
     parser = argparse.ArgumentParser(epilog=example_text, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-r', action ='store', dest='reqfile', help="SSRF Request file")
